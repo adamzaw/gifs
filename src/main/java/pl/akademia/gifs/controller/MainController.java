7f7f8f63b2e15cac8c8b08fd3e3f843b1424e7f1
Adam Zawadka <zawadka.adam@gmail.com>
2020-02-16 11:28:19 +0100

    second commit

---
+++ src/main/java/pl/akademia/gifs/controller/MainController.java
@@ -0,0 +1,17 @@
+package pl.akademia.gifs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello(){
+        return "dziala";
+    }
+
+}
